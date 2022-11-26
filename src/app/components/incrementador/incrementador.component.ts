e19fba41c2135f37b77f0e22ac62fb9178587f29
Maikol <maikolsoro.z1998@gmail.com>
2022-11-26 15:10:48 -0800

    Create the component incrementer

---
+++ src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html'
+})
+export class IncrementadorComponent {
+   progreso: number = 50;
+
+  get getPorcentaje() {
+    return `${ this.progreso }%`;
+  }
+
+  cambiarValor(valor: number){
+
+    if( this.progreso >= 100 && valor >= 0){
+     return this.progreso = 100;
+    }
+    if( this.progreso <= 0 && valor < 0){
+      return this.progreso = 0;
+     }
+   return  this.progreso = this.progreso + valor;
+  }
+}
