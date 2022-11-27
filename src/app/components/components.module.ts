a08017a25d8d08a9d31bbc0bfb4835373f5c50a6
Maikol <maikolsoro.z1998@gmail.com>
2022-11-26 16:16:39 -0800

    Conditionally button color and details

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IncrementadorComponent } from './incrementador/incrementador.component';
+import IncrementadorComponent from './incrementador/incrementador.component';
 import { FormsModule } from '@angular/forms';
 
 
--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,12 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html'
 })
-export class IncrementadorComponent {
+export default class IncrementadorComponent implements OnInit {
+
+  ngOnInit() {
+      this.btnClass = `btn ${ this.btnClass }`;
+  }
 
   @Input('valor') progreso: number = 50;
+  @Input() btnClass: string = 'btn-primary';
 
   @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
@@ -22,5 +27,16 @@ export class IncrementadorComponent {
      }
      this.progreso = this.progreso + valor;
     return this.valorSalida.emit(this.progreso + valor);
+  }
+  onChange(newValor: number){
+
+    if(newValor >= 100) {
+      this.progreso = 100;
+    } else if(newValor <= 0) {
+      this.progreso = 0;
+    } else {
+      this.progreso = newValor;
+    }
+    this.valorSalida.emit( this.progreso );
   }
 }
